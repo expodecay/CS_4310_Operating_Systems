47246f0b0f0f11bc55a1fb72f7585bd5122dd85b
Rick Ramirez <Rick.ramirez6174@gmail.com>
2021-03-09 14:05:31 -0800

    git init

--- src/version_1_2_test.java
+++ src/version_1_2_test.java
@@ -12,7 +12,9 @@ public class version_1_2_test
     private static version_1[] linkedPCB    = new version_1[10];
     private static version_2[] nonLinkedPCB = new version_2[10];
 
-    //*  *//
+    //* Assume that PCB[0] is the only currently existing process and write 4
+    //test program that performs a series of process creations and
+    //destructions. An example of such a test program is given below.  *//
 
     public static void main(String[] args) {
 
