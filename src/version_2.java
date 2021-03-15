137aae1a785adc96c48e00fc0920b8881a7b478b
rfkcpp <56742706+rfkcpp@users.noreply.github.com>
2021-03-14 23:13:07 -0700

    Add files via upload

--- src/version_2.java
+++ src/version_2.java
@@ -58,8 +58,28 @@ public class version_2 implements processInterface
      * @param newEntry the object to be added as a new entry
      * @return true if the creation is successful
      */
-    public boolean create(int newEntry) {
-        return false;
+    public version_2 create(int newEntry) {
+        
+    	int older_sibling = this.get_index_of_youngest_child();
+    	
+    	version_2 newPCB = new version_2(newEntry);
+    	newPCB.set_first_child(Integer.MIN_VALUE);
+    	newPCB.set_younger_sibling(Integer.MIN_VALUE);
+    	newPCB.set_older_sibling(older_sibling);
+    	
+    	
+    	OsSystem.put_nonLinkedPCB(newPCB, OsSystem.get_free_slot_nonLinked());
+    	
+    	if (this.firstChild == Integer.MIN_VALUE)
+    		this.set_first_child(OsSystem.get_index_of(newPCB));
+    	
+    	//setting the younger sibling of the older sibling to index of new PCB
+    	if (older_sibling>= 0 && older_sibling<=100)
+    		OsSystem.get_nonLinkedPCB_at_index(older_sibling).set_younger_sibling(OsSystem.get_index_of(newPCB));
+    	
+    	return newPCB;
+    	
+    	
     }
 
     /**
@@ -71,5 +91,34 @@ public class version_2 implements processInterface
     public static boolean destroy(int anEntry) {
         return false;
     }
+    
+    
+   
+    
+    //returns the index of the youngest child
+    public int get_index_of_youngest_child() {
+    	
+    	if (firstChild==Integer.MIN_VALUE)
+    		return Integer.MIN_VALUE;
+    
+    
+    else {
+    	int index_of_child=this.firstChild;
+    	while (index_of_child != Integer.MIN_VALUE) {
+    		if (OsSystem.get_nonLinkedPCB_at_index(index_of_child).get_younger_sibling()==Integer.MIN_VALUE)
+    			return index_of_child;
+    		else
+    			index_of_child = OsSystem.get_nonLinkedPCB_at_index(index_of_child).get_younger_sibling();
+    		
+    	}
+    	
+    	return index_of_child;
+    
+    
+    } //end of else
+    	
+
+
+    }//end of get_index_of
 }
 
